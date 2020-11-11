78847920c303c30b7bd9a988401c3625a2009fe6
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-10 23:25:30 -0500

    created SwiftUI for iOS project

---
+++ Connect4/Connect4/Connect4App.swift
@@ -0,0 +1,17 @@
+//
+//  Connect4App.swift
+//  Connect4
+//
+//  Created by Golden Thumb on 2020-11-10.
+//
+
+import SwiftUI
+
+@main
+struct Connect4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Connect4/Connect4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Connect4
+//
+//  Created by Golden Thumb on 2020-11-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Friend!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
